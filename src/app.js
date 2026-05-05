6dc6749152f8100a2c653e06ec97b6e5260dc1a2
arushi-si <arusingh666@gmail.com>
2026-05-05 22:32:01 +0530

    chore: query params & routing

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ const app = express();
 const port = 3000;
 
 // request handler
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ fName: "Arush", lName: "Singh" });
 });
 
-app.get("/hello", (req, res) => {
+app.use("/hello/:hId", (req, res) => {
+  console.log(req.params);
   res.send("Hello Hellooo!");
 });
 
+// Advance routing: ? + *
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
